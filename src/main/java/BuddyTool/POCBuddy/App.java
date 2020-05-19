4db3a44158127e9bc3087d3937794d59d929152c
AparnaGutta426 <aparna.gutta426@gmail.com>
2020-05-19 09:10:01 +0000

    Test Commit

---
+++ src/main/java/BuddyTool/POCBuddy/App.java
@@ -0,0 +1,13 @@
+package BuddyTool.POCBuddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
