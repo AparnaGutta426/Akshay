169abde555a76353d068e7f1a19a498d6e8db877
AparnaGutta426 <aparna.gutta426@gmail.com>
2020-05-19 14:06:09 +0000

    edited chromedriver

--- src/test/java/BuddyTool/POCBuddy/AppTest.java
+++ src/test/java/BuddyTool/POCBuddy/AppTest.java
@@ -32,7 +32,7 @@ public class AppTest {
 	@BeforeClass
 	public void testSetUp() {
 		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Buddy\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Chrome\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Chrome/chromedriver.exe");
 		System.out.println(System.getProperty("user.dir")+"/Chrome/chromedriver.exe");
 		driver = new ChromeDriver();
 	}
